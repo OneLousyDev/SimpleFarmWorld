d083051fd755471b0ea4ed0427b83cc9268d65ee
OneLousyDev <lousy@lousiesmods.com>
2021-11-23 15:40:01 +0000

    Make experimental settings warning disappear

---
+++ src/main/java/com/lousiesmods/simplefarmworld/mixin/client/MixinServerWorldInfo.java
@@ -0,0 +1,20 @@
+package com.lousiesmods.simplefarmworld.mixin.client;
+
+import com.mojang.serialization.Lifecycle;
+
+import net.minecraft.world.storage.ServerWorldInfo;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ServerWorldInfo.class)
+public class MixinServerWorldInfo 
+{
+	@Inject(method = "getLifecycle", at = @At("HEAD"), cancellable = true)
+	private void forceStableLifeCycle(CallbackInfoReturnable<Lifecycle> cir) 
+	{
+		cir.setReturnValue(Lifecycle.stable());
+	}
+}
