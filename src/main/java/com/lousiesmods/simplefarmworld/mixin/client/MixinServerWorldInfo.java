88b062dfd11f7dd37a186b2d0ae6aa126d15d44c
OneLousyDev <lousy@lousiesmods.com>
2021-11-23 16:50:13 +0000

    Disable mixins temporarily to prevent world crash

--- src/main/java/com/lousiesmods/simplefarmworld/mixin/client/MixinServerWorldInfo.java
+++ src/main/java/com/lousiesmods/simplefarmworld/mixin/client/MixinServerWorldInfo.java
@@ -7,14 +7,15 @@ import net.minecraft.world.storage.ServerWorldInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerWorldInfo.class)
-public class MixinServerWorldInfo 
+public abstract class MixinServerWorldInfo 
 {
-	@Inject(method = "getLifecycle", at = @At("HEAD"), cancellable = true)
-	private void forceStableLifeCycle(CallbackInfoReturnable<Lifecycle> cir) 
-	{
-		cir.setReturnValue(Lifecycle.stable());
-	}
+	//todo this @Inject(method = "getLifecycle", at = @At("HEAD"), cancellable = true)
+	//private void forceStableLifeCycle(CallbackInfoReturnable<Lifecycle> cir) 
+	//{
+		//cir.setReturnValue(Lifecycle.stable());
+	//}
 }
